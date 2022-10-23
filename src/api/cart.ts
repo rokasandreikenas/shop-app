84c94a0fd9ebcd7208f588f5a52220b1baf7f588
Rokas Andreikenas <shyptt@gmail.com>
2022-10-23 21:53:14 +0300

    Update cartItem API's

--- src/api/cart.ts
+++ src/api/cart.ts
@@ -1,19 +1,31 @@
 import axios from 'axios';
 import {api} from '../consts/api';
+import {CartItemDefinition} from '../types/cart';
 import {ItemDefinition} from '../types/item';
 
 export const getCartItems = () => {
   return axios.get(`${api}/cart`).then(res => res.data);
 };
 
-export const addItemToCart = (item: ItemDefinition) => {
-  return axios.post(`${api}/cart`, {item}).then(res => res.data);
+export const addCartItem = (id: ItemDefinition['id'], quantity = 1) => {
+  return axios
+    .post(`${api}/cart`, {}, {params: {id, quantity}})
+    .then(res => res.data);
 };
 
 export const resetCartItems = () => {
   return axios.delete(`${api}/cart`).then(res => res.data);
 };
 
-export const deleteItemFromCart = (id: ItemDefinition['id']) => {
+export const deleteCartItem = (id: ItemDefinition['id']) => {
   return axios.delete(`${api}/cart/${id}`).then(res => res.data);
+};
+
+export const updateCartItem = (
+  id: ItemDefinition['id'],
+  quantity: number,
+): Promise<CartItemDefinition> => {
+  return axios
+    .put(`${api}/cart/${id}`, {}, {params: {quantity}})
+    .then(res => res.data);
 };
