035a9fc1415265c042621cda9a69bb66b4134640
Rokas Andreikenas <shyptt@gmail.com>
2022-10-23 22:22:31 +0300

    Remove unused prop

--- src/types/routes.ts
+++ src/types/routes.ts
@@ -1,10 +1,8 @@
-import {UseMutationResult} from '@tanstack/react-query';
 import {ItemDefinition} from './item';
 
 export type RootStackParamList = {
   Home: undefined;
   Item: {
     item: ItemDefinition;
-    mutation: UseMutationResult<any, unknown, number, unknown>;
   };
 };
