40d301a1a54d8b62e54aa4194261baed3045c6c7
Rokas Andreikenas <shyptt@gmail.com>
2022-10-23 20:13:26 +0300

    Add react-query & axios

--- App.tsx
+++ App.tsx
@@ -1,8 +1,15 @@
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import React from 'react';
 import AppNavigation from './src/navigations/AppNavigation';
 
+const queryClient = new QueryClient();
+
 const App = () => {
-  return <AppNavigation />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppNavigation />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
