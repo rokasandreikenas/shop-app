44eed7bd60a74bcb1d7e40551261adf9c2b08269
Rokas Andreikenas <rokas.andreikenas@exacaster.com>
2022-10-24 21:05:07 +0300

    Make pressable image

--- src/components/CartItem/index.tsx
+++ src/components/CartItem/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, TouchableOpacity, View} from 'react-native';
 import {CartItemDefinition} from '../../types/cart';
 import CartItemCounter from './CartItemCounter';
 import CartItemDetails from './CartItemDetails';
@@ -8,14 +8,17 @@ import CartItemImage from './CartItemImage';
 interface Props {
   cartItem: CartItemDefinition;
   isLoading: boolean;
+  openItemDetails: () => void;
   handleUpdateQuantity: (id: CartItemDefinition['item_id'], quantity: number, type: 'reduce' | 'increase') => void;
 }
 
-const CartItem = ({cartItem, isLoading, handleUpdateQuantity}: Props) => {
+const CartItem = ({cartItem, isLoading, openItemDetails, handleUpdateQuantity}: Props) => {
   return (
     <View style={styles.container}>
       <View style={styles.leftSide}>
-        <CartItemImage image={cartItem.image} />
+        <TouchableOpacity onPress={openItemDetails}>
+          <CartItemImage image={cartItem.image} />
+        </TouchableOpacity>
         <CartItemCounter cartItem={cartItem} isLoading={isLoading} handleUpdateQuantity={handleUpdateQuantity} />
       </View>
       <View style={styles.rightSide}>
